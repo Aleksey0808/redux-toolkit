3173d43aaf0b0b8f4ecae0482b4e4386eabe8cdd
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-03-30 18:40:42 +0300

    230330

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,28 +1,24 @@
-import { Link } from "react-router-dom"
-import { UserMenu } from "./UserMenu"
-import { useSelector } from "react-redux"
+import { UserMenu } from './UserMenu';
+import { useSelector } from 'react-redux';
+import { LoginForm } from './LoginForm';
 
 export const AppBar = () => {
-    const isLoggedIn = useSelector(state => state.user.isLoggedIn)
+  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
 
-    return <header style={
-        {
+  return (
+    <header
+      style={{
         display: 'flex',
-        justifyContent: "space-between",
+        justifyContent: 'space-between',
         padding: 8,
         borderBottom: '1px solid blqck',
-        marginBottom: 12,}
-    }> 
-        {/* <nav>
-            <Link>Login</Link>
-        </nav>
-    {isLoggedIn && <UserMenu />} */}
-
-    <nav>
-        {!isLoggedIn && <Link to="/login">Log in</Link>}
-        
+        marginBottom: 12,
+      }}
+    >
+      <nav>
+        <LoginForm />
       </nav>
       {isLoggedIn && <UserMenu />}
-    
     </header>
-}
+  );
+};
