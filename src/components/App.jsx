a984ab288b8de36dc023e801cd3b306ede258a78
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2023-03-30 18:15:07 +0300

    fix/bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../redux/myValue/slice';
-import { add, remove } from '../redux/item/slice';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { increment, decrement } from '../redux/myValue/slice';
+// import { add, remove } from '../redux/item/slice';
 
 const Login = lazy(() => import('../pages/loginPage'));
 const Home = lazy(() => import('../pages/Home'));
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const value = useSelector(state => state.myValue);
-  const item = useSelector(state => state.items);
+  // const dispatch = useDispatch();
+  // const value = useSelector(state => state.myValue);
+  // const item = useSelector(state => state.items);
 
   return (
     <>
